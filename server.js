095639b7faa7dcb16727e745d719223075ee7ef5
AbdelI <homeworkoracle@protonmail.com>
2020-01-20 16:34:50 +0100

    server,index running

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
+// const helmet = require("helmet");
+// const db = require("./data/dbConfig.js");
 
-const db = require('./data/dbConfig.js');
+const accRouter = require("./account-router");
 
 const server = express();
 
 server.use(express.json());
+
+// server.use(helmet());
+
+server.use("/api/accounts", accRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's do some accounting</h2>`);
+});
 
 module.exports = server;
